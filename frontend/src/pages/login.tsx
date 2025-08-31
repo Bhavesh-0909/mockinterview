9e8261a6990bcfe58fa5a5e565b0baf015832985
Bhavesh Choudhary <bhaveshchoudhary0909@gmail.com>
2025-08-31 15:11:16 +0530

    feat: update login page layout and remove unused orbiting circles component

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,7 +1,5 @@
 import { Bot } from "lucide-react"
 import { LoginForm } from "@/components/login-form";
-import { OrbitingCircles } from "@/components/magicui/orbiting-circles";
-import { File, Settings, Search } from "lucide-react";
 
 export default function LoginPage() {
   return (
@@ -22,21 +20,6 @@ export default function LoginPage() {
         </div>
       </div>
       <div className="bg-muted relative hidden lg:flex lg:justify-center lg:items-center ">
-        <div className="h-full w-full p-10">
-            <div className="relative overflow-hidden h-[500px] w-full">
-                <OrbitingCircles>
-                    <File />
-                    <Settings />
-                    <File />
-                </OrbitingCircles>
-                <OrbitingCircles radius={100} reverse>
-                    <File />
-                    <Settings />
-                    <File />
-                    <Search />
-                </OrbitingCircles>
-            </div>
-        </div>
       </div>
     </div>
   )

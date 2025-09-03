f7d8d29a15752b193ece6b9a43b35ac33a739197
Bhavesh Choudhary <bhaveshchoudhary0909@gmail.com>
2025-09-03 18:04:10 +0530

    feat: add Footer component to Home page for improved layout and branding

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div className="w-full flex flex-col items-center justify-center p-4 border-t mt-4">
+        <h3 className="text-center text-md text-muted-foreground leading-7 [&:not(:first-child)]:mt-6">Build with ❣️ by <a href="https://github.com/Bhavesh-0909" className="underline-offset-4 underline scroll-m-20 tracking-tight text-primary">Bhavesh Choudhary</a></h3>
+        <p className="text-center text-sm text-muted-foreground leading-7 [&:not(:first-child)]:mt-3">© 2024 MockInterview. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,15 @@
 import HeroSection from "@/components/hero-section";
 import MarqueeAnimation from "@/components/MarqueeAnimation";
 import Testimonial from "@/components/testimonial.tsx";
+import Footer from "@/components/Footer";
+
 function Home() {
   return (
     <div className="min-h-[calc(100vh-4rem)] max-w-screen flex flex-col">
       <HeroSection />
       <MarqueeAnimation />
       <Testimonial />
+      <Footer />
     </div>
   )
 }

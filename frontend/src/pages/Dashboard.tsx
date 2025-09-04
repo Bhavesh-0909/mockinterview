15586afe0140357694758bd979ff507cbd5bb017
Bhavesh Choudhary <bhaveshchoudhary0909@gmail.com>
2025-09-04 23:15:37 +0530

    fix: implement user loading state and enhance dashboard layout with user details

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,21 @@
-
+import { useAuth } from "@/context/AuthContext"
 function Dashboard() {
+  const { user } = useAuth();
+  if(!user) {
+    return <p>Loading...</p>
+  }
   return (
-    <div>Dashboard</div>
+    <div className="flex flex-col justify-center space-y-4 pr-5">
+      <h1 className="text-md scroll-m-20 tracking-tight text-accent-foreground ml-1">Dashboard</h1>
+      <p className="text-sm text-muted-foreground ml-1">Welcome, {user.fullname || user.email}!</p>
+      <div className="flex items-center w-full gap-2">
+        <img src={user.avatarlink} alt="User Avatar" className="h-16 object-cover bg-secondary w-16 rounded-full" />
+        <div className="flex flex-col justify-between space-y-1">
+          <h3 className="text-lg scroll-m-20 text-center text-balance font-bold tracking-tight uppercase">{user.fullname || user.email}</h3>
+          <p className="text-xs text-muted-foreground">{user.email}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
